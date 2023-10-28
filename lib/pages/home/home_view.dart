c5f5581d4bdef1521859f1289d5c0dc81af26298
bstawy <125200508@sha.edu.eg>
2023-10-28 14:36:19 +0200

    delete unused import

--- lib/pages/home/home_view.dart
+++ lib/pages/home/home_view.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:news/models/category_model.dart';
 import 'package:news/pages/home/widgets/custom_drawer.dart';
