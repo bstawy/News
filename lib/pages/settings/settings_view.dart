aa1f68b19d27b6d8b567ed53c2cb6aa805dc0629
bstawy <125200508@sha.edu.eg>
2023-10-28 14:34:53 +0200

    create settings view

--- lib/pages/settings/settings_view.dart
+++ lib/pages/settings/settings_view.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
 
+import '../home/widgets/custom_drawer.dart';
+
 class SettingsView extends StatelessWidget {
   static const String routeName = 'settings';
   const SettingsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    var mediaQuery = MediaQuery.of(context).size;
+    var width = mediaQuery.width;
+    var height = mediaQuery.height;
+    var theme = Theme.of(context);
+
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        image: DecorationImage(
+          image: AssetImage('assets/images/background_pattern.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Settings')),
+        drawer: const CustomDrawer(),
+        body: Padding(
+          padding: EdgeInsets.only(
+              left: width * 0.08, right: width * 0.08, top: height * 0.03),
+          child: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [],
+          ),
+        ),
+      ),
+    );
   }
 }
