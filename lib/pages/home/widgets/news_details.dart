ad28f9537cabc7a30240c028b097e0ba9501d223
bstawy <125200508@sha.edu.eg>
2023-10-31 23:42:08 +0200

    use mvvm pattern in home View

--- lib/pages/home/widgets/news_details.dart
+++ lib/pages/home/widgets/news_details.dart
@@ -5,10 +5,10 @@ import '../../../models/source_model.dart';
 import 'news_article_list.dart';
 
 class NewsDetails extends StatefulWidget {
-  final SourceModel sourceModel;
+  final List<Source> sourcesList;
   const NewsDetails({
     super.key,
-    required this.sourceModel,
+    required this.sourcesList,
   });
 
   @override
@@ -21,7 +21,7 @@ class _NewsDetailsState extends State<NewsDetails> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: widget.sourceModel.sources.length,
+      length: widget.sourcesList.length,
       child: Column(
         children: [
           TabBar(
@@ -36,19 +36,19 @@ class _NewsDetailsState extends State<NewsDetails> {
               ),
             ),
             isScrollable: true,
-            tabs: widget.sourceModel.sources
+            tabs: widget.sourcesList
                 .map(
                   (e) => TabBarItem(
                     title: e.name,
                     isSelected:
-                        selectedIndex == widget.sourceModel.sources.indexOf(e),
+                        selectedIndex == widget.sourcesList.indexOf(e),
                   ),
                 )
                 .toList(),
           ),
           Expanded(
             child: NewsArticleList(
-              source: widget.sourceModel.sources[selectedIndex],
+              source: widget.sourcesList[selectedIndex],
             ),
           ),
         ],
--- lib/pages/home/widgets/news_view.dart
+++ lib/pages/home/widgets/news_view.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:news/core/network_layer/api_manager.dart';
 import 'package:news/models/category_model.dart';
-
-import '../../../models/source_model.dart';
-import 'news_details.dart';
+import 'package:news/pages/home/widgets/news_details.dart';
+import 'package:provider/provider.dart';
+import '../home_view_model.dart';
 
 class NewsView extends StatefulWidget {
   final CategoryModel categoryModel;
+
   const NewsView({
     super.key,
     required this.categoryModel,
@@ -17,8 +17,24 @@ class NewsView extends StatefulWidget {
 }
 
 class _NewsViewState extends State<NewsView> {
+  var vm = HomeViewModel();
+
+  @override
+  void initState() {
+    vm.getSource(widget.categoryModel.id);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => vm,
+      builder: (context, child) => NewsDetails(
+        sourcesList: vm.sourcesList,
+      ),
+    );
+
+    /*
     return FutureBuilder<SourceModel>(
         future: ApiManager.fetchSources(widget.categoryModel.id),
         builder: (context, snapshot) {
@@ -35,6 +51,6 @@ class _NewsViewState extends State<NewsView> {
           return NewsDetails(
             sourceModel: sourceModel,
           );
-        });
+        });*/
   }
 }
