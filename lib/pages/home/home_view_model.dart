da4e389c94715dc43b699e11d9f9f76e86d0042f
bstawy <125200508@sha.edu.eg>
2023-10-31 23:41:46 +0200

    use mvvm pattern {create home view model}

---
+++ lib/pages/home/home_view_model.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:news/core/network_layer/api_manager.dart';
+
+import '../../models/source_model.dart';
+
+class HomeViewModel extends ChangeNotifier{
+
+  List<Source> _sourcesList = [];
+
+  List<Source> get sourcesList => _sourcesList;
+
+  getSource(String categoryId) async {
+
+    try{
+      var response = await ApiManager.fetchSources(categoryId);
+
+      if(response.status == "ok") {
+        _sourcesList = response.sources;
+      }
+    } catch(e) {
+      print(e.toString());
+    }
+
+  }
+
+
+}
